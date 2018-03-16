f7e0c733b261b94c92ad819b1af8b346313c1b23
Anastasiya Garbuz <36642340+StationToStation@users.noreply.github.com>
2018-03-16 13:00:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-    if (array.length == 0) return 0;
+  if (array.length == 0) return 0;
 
-    let longestStreak = 1;
-    for (let i = 0; i < array.length; i++) {
-        let left = array[i] - 1,
-            right = array[i] + 1,
-            streak = 1;
+  let longestStreak = 1;
+  let set = new Set(array);
 
-        while (array.indexOf(left) != -1) {
-            streak++;
-            array.splice(array.indexOf(left), 1);
-            left--;
-        }
-        while (array.indexOf(right) != -1) {
-            streak++;
-            array.splice(array.indexOf(right), 1);
-            right++;
-        }
-        longestStreak = (longestStreak > streak) ? longestStreak : streak;
-    }
-    return longestStreak;
+  for (let number of set.values()) {
+      let compared = number - 1,
+          streak = 1;
+      while (set.has(compared)) {
+          set.delete(compared);
+          compared--;
+          streak++;
+      }
+      compared = number + 1;
+      while (set.has(compared)) {
+          set.delete(compared);
+          compared++;
+          streak++;
+      }
+      longestStreak = (streak > longestStreak) ? streak : longestStreak;
+  }
+  return longestStreak;
 }
